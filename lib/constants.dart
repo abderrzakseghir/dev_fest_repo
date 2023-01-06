ff90f12ed1ce8cfac6ca970c10d44d0a9b2647d4
asegh <a.seghir@esi-sba.dz>
2023-01-06 22:02:49 +0100

    1st commit

---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const kDark = Color(0xFF121212);
+const kDark1 = Color(0xFF212121);
+const kDark2 = Color(0xFF535353);
+const kDar3 = Color(0xFFB3B3B3);
+
+const kGray1 = Color(0xFF98A2B3);
+const kGray2 = Color(0xFFD0D5DD);
+
+const kWhite1 = Color(0xFFE4E7EC);
+const kWhite2 = Color(0xFFF2F4F7);
+
+
+const kGreen1 = Color(0xFF1BA351);
+const kGreen2 = Color(0xFF4CC772);
+const kGreen3 = Color(0xFF7CF59C);
+const kGreen4 = Color(0xFF12B76A);
+
+const kRed= Color(0xFFF04438);
+const kOrange = Color(0xFFF79009);
+
+
+
+
